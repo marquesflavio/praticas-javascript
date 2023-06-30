8b64a70739ea90d00c9f13ac63462f9407fb31cb
Flávio Marques <flaviomarques2@hotmail.com>
2023-06-29 21:29:26 -0300

    atualização da pasta ggv

--- API/Gabi-ggv/main.js
+++ API/Gabi-ggv/main.js
@@ -1,14 +1,18 @@
-fetch('https://api.ggvinteligencia.com.br/webhook/start-test', {
+fetch("https://api.ggvinteligencia.com.br/webhook/start-test", {
 
     method : "GET",
-    mode: "no-cors",
-    Headers : {
-        "authorization": "teste-dev-ggv"
+    headers : {
+        "Authorization": "teste-dev-ggv"
     }
+    
+
 }
 )
 
-.then((res) => res.json())
-.then((data)=>{
-    console.log(data)
+.then((res) => console.log(res.json()))
+
+.catch((err) => {
+    console.log("ocorreu o erro: ",err)
 })
+
+// está dando erro de CORS, como se não tivesse autorização para acessar os dados do endpoint.
